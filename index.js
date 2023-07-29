11c51a4f61350e199ecde3c9f9330d3ba2a02168
Kozar <Kevin.Kozar@avl.com>
2023-07-29 18:24:51 +0200

    Adding API Functionality

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import axios from "axios";
 
 const app = express();
 const port = 3000;
+const apiKey = "40a40239479547f0bf2154453232907";
+const baseUrl = "http://api.weatherapi.com/v1/current.json?key="+apiKey+"&q=";
 
 let locArr = [];
 
@@ -17,17 +19,25 @@ app.get("/", async (req, res) => {
 
 app.post("/", async (req, res) => {
   console.log("clicked")
-  const content = req.body.location
+  const location = req.body.location
+  const temperature = "25°";
+  const icon = "X";
   const gridItemClass = "grid-item";
-  const gridItem = `<div class=${gridItemClass}>
-                        <div class="grid-item1">${content}</div>
-                        <div class="grid-item2"></div>
-                        <div class="grid-item3"></div>
-                        <div class="grid-item4"></div>
-                    </div>`;
-  locArr.push(gridItem)
-  console.log(locArr);
-  res.render("index.ejs", {data: locArr})
+
+  try {
+    const response = await axios.get(baseUrl+location)
+    const gridItem = `<div class=${gridItemClass}>
+                          <div class="grid-item1 flex-center"><img src="${response.data.current.condition.icon}"></img></div>
+                          <div class="grid-item2 flex-center"><h1>${response.data.location.name}</h1></div>
+                          <div class="grid-item3 flex-center"><h1>${response.data.current.temp_c}°</h1></div>
+                      </div>`;
+    locArr.push(gridItem)
+    console.log(locArr);
+    res.render("index.ejs", {data: locArr})
+    console.log(response);
+  } catch(error) {
+    console.log(error);
+  }
 });
 
 app.listen(port, () => {
